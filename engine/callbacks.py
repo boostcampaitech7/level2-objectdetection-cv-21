e4c59da96e1010e80c475ff7f8e36f99463e2681
Haneol-Kijm <haneol.kijm@gmail.com>
2024-10-02 13:49:32 +0900

    💡 Add original code comment for reference

--- engine/callbacks.py
+++ engine/callbacks.py
@@ -69,6 +69,56 @@ class PredictionCallback(Callback):
         submission.to_csv(output_path, index=None, lineterminator="\n")
         print(submission.head())
         print(f"Output csv file successfully saved in {output_path}!!")
+
+    # 참고용 베이스라인 코드 원본
+    # def main():
+    #     annotation = '/data/ephemeral/home/dataset/test.json' # annotation 경로
+    #     data_dir = '/data/ephemeral/home/dataset' # dataset 경로
+    #     test_dataset = CustomDataset(annotation, data_dir)
+    #     score_threshold = 0.05
+    #     check_point = './checkpoints/faster_rcnn_torchvision_checkpoints.pth' # 체크포인트 경로
+        
+
+    #     test_data_loader = DataLoader(
+    #         test_dataset,
+    #         batch_size=8,
+    #         shuffle=False,
+    #         num_workers=4
+    #     )
+    #     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    #     print(device)
+        
+    #     # torchvision model 불러오기
+    #     model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+    #     num_classes = 11  # 10 class + background
+    #     # get number of input features for the classifier
+    #     in_features = model.roi_heads.box_predictor.cls_score.in_features
+    #     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+    #     model.to(device)
+    #     model.load_state_dict(torch.load(check_point))
+    #     model.eval()
+        
+    #     outputs = inference_fn(test_data_loader, model, device)
+    #     prediction_strings = []
+    #     file_names = []
+    #     coco = COCO(annotation)
+
+    #     # submission 파일 생성
+    #     for i, output in enumerate(outputs):
+    #         prediction_string = ''
+    #         image_info = coco.loadImgs(coco.getImgIds(imgIds=i))[0]
+    #         for box, score, label in zip(output['boxes'], output['scores'], output['labels']):
+    #             if score > score_threshold: 
+    #                 # label[1~10] -> label[0~9]
+    #                 prediction_string += str(label-1) + ' ' + str(score) + ' ' + str(box[0]) + ' ' + str(
+    #                     box[1]) + ' ' + str(box[2]) + ' ' + str(box[3]) + ' '
+    #         prediction_strings.append(prediction_string)
+    #         file_names.append(image_info['file_name'])
+    #     submission = pd.DataFrame()
+    #     submission['PredictionString'] = prediction_strings
+    #     submission['image_id'] = file_names
+    #     submission.to_csv('./faster_rcnn_torchvision_submission.csv', index=None)
+    #     print(submission.head())
         
 
 
