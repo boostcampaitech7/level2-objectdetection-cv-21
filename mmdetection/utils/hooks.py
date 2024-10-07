eb403362ce313a3636ac80d43cdf6859dae950d2
Haneol-Kijm <haneol.kijm@gmail.com>
2024-10-07 23:47:33 +0900

    🎨 Add customized wandb logger hook to prevent from wandb init run twice

---
+++ mmdetection/utils/hooks.py
@@ -0,0 +1,32 @@
+import warnings
+from typing import Dict, Optional
+
+from mmcv.runner.hooks import Hooks
+from mmcv.runner.hooks.logger.wandb import WandbLoggerHook
+
+@HOOKS.register_module()
+class NoInitWandbLoggerHook(WandbLoggerHook):
+    def __init__(self, 
+                 init_kwargs: Optional[Dict] = None, 
+                 interval: int = 10, 
+                 ignore_last: bool = True,
+                 reset_flag: bool = False,
+                 commit: bool = True,
+                 by_epoch: bool = True,
+                 with_step=False, log_artifact=False, out_suffix=None):
+        super().__init__(init_kwargs=init_kwargs, interval=interval, ignore_last=ignore_last, reset_flag=reset_flag, commit=commit, by_epoch=by_epoch, with_step=with_step, log_artifact=log_artifact, out_suffix=out_suffix)
+
+    def before_run(self, runner) -> None:
+        super(WandbLoggerHook, self).before_run(runner)
+        if self.wandb is None:
+            self.import_wandb()
+        print("Warning: Wandb not initialized in the hook. Ensure initialization before hook execution.")
+        summary_choice = ['min', 'max', 'mean', 'best', 'last', 'none']
+        if self.define_metric_cfg is not None:
+            for metric, summary in self.define_metric_cfg.items():
+                if summary not in summary_choice:
+                    warnings.warn(
+                        f'summary should be in {summary_choice}. '
+                        f'metric={metric}, summary={summary} will be skipped.')
+                self.wandb.define_metric(  # type: ignore
+                    metric, summary=summary)
