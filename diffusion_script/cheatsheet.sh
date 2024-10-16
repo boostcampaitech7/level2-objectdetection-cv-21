67b479997f6acfe7c8e1409500c48ab6a8101fcb
Haneol-Kijm <haneol.kijm@gmail.com>
2024-10-16 18:35:31 +0900

    🚚 REname some paths

--- diffusion_script/cheatsheet.sh
+++ diffusion_script/cheatsheet.sh
@@ -1,9 +1,9 @@
 python3 generate_with_imprior.py \
-        -a "/media/data/coco_fsod/seed1/10shot_novel.json" \
-        -i /media/data/coco_fsod/train2017/ \
-        -e "/media/data/ControlAug/cnet/experiments/coco10s1_512p" \
+        -a "/data/ephemeral/home/dataset/train.json" \
+        -i /data/ephemeral/home/dataset/ \
+        -e "/data/ephemeral/home/media/data/ControlAug/cnet/experiments/coco10s1_512p" \
         -l 4883 \
-        -s 1 \
+        -s 2 \
         -p 512 \
         -m 0 \
         --vpg_mode HED \
