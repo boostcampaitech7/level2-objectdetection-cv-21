f66a1ad2cf9ac596918f88753d45263372ee34ab
boyamie <bhkim4550@pusan.ac.kr>
2024-10-16 15:19:13 +0900

    🚀 Deploy yolov11x

--- yolov11/utils/convert_json_to_yolo.py
+++ yolov11/utils/convert_json_to_yolo.py
@@ -19,16 +19,17 @@ def convert_coco_to_yolo(json_path, img_folder):
             for ann in data['annotations']:
                 if ann['image_id'] == img_id:
                     category_id = ann['category_id'] - 1  # 클래스 ID가 1부터 시작한다고 가정
-                    bbox = ann['bbox']
+                    if category_id >= 0:  # 클래스 ID가 0 이상인 경우에만 저장
+                        bbox = ann['bbox']
 
-                    # YOLO 형식으로 변환 (중심 좌표 및 너비, 높이를 정규화)
-                    x_center = (bbox[0] + bbox[2] / 2) / width
-                    y_center = (bbox[1] + bbox[3] / 2) / height
-                    w = bbox[2] / width
-                    h = bbox[3] / height
+                        # YOLO 형식으로 변환 (중심 좌표 및 너비, 높이를 정규화)
+                        x_center = (bbox[0] + bbox[2] / 2) / width
+                        y_center = (bbox[1] + bbox[3] / 2) / height
+                        w = bbox[2] / width
+                        h = bbox[3] / height
 
-                    label_f.write(f"{category_id} {x_center} {y_center} {w} {h}\n")
+                        label_f.write(f"{category_id} {x_center} {y_center} {w} {h}\n")
 
 # 학습 및 테스트 데이터를 YOLO 형식으로 변환
-convert_coco_to_yolo('dataset/train.json', 'dataset/train')
-convert_coco_to_yolo('dataset/test.json', 'dataset/test')
+convert_coco_to_yolo('/data/ephemeral/home/dataset/train.json', '/data/ephemeral/home/dataset/')
+convert_coco_to_yolo('/data/ephemeral/home/dataset/test.json', '/data/ephemeral/home/dataset/')
